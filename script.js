52048532d1f58e5c1d40ab882f57befcd742afd4
23Tor <trmorton505@gmail.com>
2023-08-08 11:01:49 -0400

    Added slider, moved mouseover event to makeRows

--- script.js
+++ script.js
@@ -4,27 +4,36 @@ const container = document.getElementById("grid");
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
+    container.innerHTML = ''; // Clear previous cells
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
-    };
-};
+    }
 
-makeRows(16, 16);
-
-// Event listener for hovering over grid item
-const cells = document.querySelectorAll('.grid-item');
-
-cells.forEach(cells => {
-    cells.addEventListener('mouseover', () => {
-        cells.style.background = 'black'
+    // Add event listener for hovering over grid items
+    container.addEventListener('mouseover', (event) => {
+        if (event.target.classList.contains('grid-item')) {
+            event.target.style.background = 'black';
+        }
     });
-});
+}
+
+
+// Default grid
+const DEFAULT_ROWS = 16
+const DEFAULT_COLUMNS = 16
+makeRows(DEFAULT_ROWS, DEFAULT_COLUMNS)
+
+// Slider value box
+function updateTextInput(val) {
+    document.getElementById('sliderInput').value = val;
+    makeRows(val, val);
+}
 
 // Clear button
 const clearButton = document.querySelector('#clearButton')
-
 clearButton.addEventListener('click', () => {
+    const cells = document.querySelectorAll(".grid-item")
     cells.forEach(cells => {
         cells.style.background = '';
     });
