1db65accf34c09ab72a7908839d44b854e8a0761
23Tor <trmorton505@gmail.com>
2023-08-09 11:21:48 -0400

    Events and functions for dynamic processes

--- script.js
+++ script.js
@@ -1,40 +1,99 @@
-// Create grid
-const container = document.getElementById("grid");
+// Default values
+const DEFAULT_ROWS = 16;
+const DEFAULT_COLUMNS = 16;
 
+// Declare selectors
+const modeButton = document.querySelectorAll('.mode');
+const blackModeButton = document.querySelector(".black");
+const clearButton = document.querySelector('.clear');
+const container = document.getElementById("grid");
+const rangeInput = document.getElementById("rangeInput");
+const sliderInput = document.getElementById("sliderInput");
+let currentMode = 'black';
+
+// Event listeners
+modeButton.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.classList.contains('color')) {
+            currentMode = 'color';
+        } else if (button.classList.contains('clear')) {
+            currentMode = 'black';
+            clearGrid();
+        }
+    });
+});
+
+blackModeButton.addEventListener('click', () => {
+    currentMode = 'black';
+});
+
+clearButton.addEventListener('click', () => {
+    currentMode = 'black';
+    clearGrid();
+});
+
+rangeInput.addEventListener('input', () => {
+    const value = rangeInput.value;
+    sliderInput.value = value;
+    makeRows(value, value);
+});
+
+sliderInput.addEventListener('change', () => {
+    const value = parseInt(sliderInput.value); // Convert the value to an integer
+    if (!isNaN(value) && value >= parseInt(rangeInput.min) && value <= parseInt(rangeInput.max)) {
+        rangeInput.value = value;
+        makeRows(value, value);
+    }
+});
+
+// Functions
+// Clear buttons
+function clearGrid() {
+    container.querySelectorAll(".grid-item").forEach(cell => {
+        cell.style.background = '';
+    });
+};
+
+// Set grid size
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     container.innerHTML = ''; // Clear previous cells
-    for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
+    
+    for (let c = 0; c < (rows * cols); c++) {
+        const cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
     }
-
-    // Add event listener for hovering over grid items
-    container.addEventListener('mouseover', (event) => {
-        if (event.target.classList.contains('grid-item')) {
-            event.target.style.background = 'black';
-        }
+    
+    container.querySelectorAll('.grid-item').forEach(gridItem => {
+        gridItem.removeEventListener('mouseover', painter);
+        gridItem.addEventListener('mouseover', painter);
     });
-}
+};
 
+// Mouseover color mode
+function painter(e) {
+    if (currentMode === 'black') {
+        e.target.style.backgroundColor = 'black';
+    } else if (currentMode === 'color') {
+        const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        e.target.style.backgroundColor = randomColor;
+    }
+};
 
-// Default grid
-const DEFAULT_ROWS = 16
-const DEFAULT_COLUMNS = 16
-makeRows(DEFAULT_ROWS, DEFAULT_COLUMNS)
+function startUp() {
+    makeRows(DEFAULT_ROWS, DEFAULT_COLUMNS);
+    modeButton.forEach(button => button.addEventListener('click', setupModeButtons));
+};
 
-// Slider value box
-function updateTextInput(val) {
-    document.getElementById('sliderInput').value = val;
-    makeRows(val, val);
-}
+function setupModeButtons() {
+    if (this.classList.contains('color')) {
+        currentMode = 'color';
+    } else if (this.classList.contains('clear')) {
+        currentMode = 'black';
+        clearGrid();
+    }
+};
 
-// Clear button
-const clearButton = document.querySelector('#clearButton')
-clearButton.addEventListener('click', () => {
-    const cells = document.querySelectorAll(".grid-item")
-    cells.forEach(cells => {
-        cells.style.background = '';
-    });
-});
+startUp();
